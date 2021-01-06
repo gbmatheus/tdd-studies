f9fff9a3af337ebebcca85e5e3bdc857f6260d00
gbmatheus <gabrielmatheus2503@gmail.com>
2021-01-06 20:11:22 -0300

    Criando desafio número de erdos

---
+++ src/challenges/numeroDeErdos/numeroDeErdos.spec.ts
@@ -0,0 +1,20 @@
+// numero de erdos = 0
+// erdos,numeroDeErdos = 0
+
+// joao.numeroDeErdos = 1 + menor numero de erdos dos matematicco
+// que escreveram um argico junto com joao
+
+// qualquerMatematico = não escreveu com erdos || outro matematico que escreveu com erdos => numero de erdos infinito
+
+// 511 grupoEscreveramComErdos.numeroDeErdos = 1; -> escreveram com erdos
+// grupoAmigosQueEscreveramComErdos.numeroDeErdos = 2;
+// grupoAmigosDosAmigosQueEscreveramComErdos = 3;
+// sucessivamente;
+
+// Escreva um programa que, dada uma lista de publicações e seus autores, calcule o número de Erdos de cada autor.
+
+describe('Número de Erdos dos matematicos', () => {
+  test('Ainda não implementado', () => {
+    expect('ok').toEqual('ok');
+  });
+});
